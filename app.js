f1cc589b100c5d1338e2970f63accbb1e3feda42
harshsingh554 <72743057+harshsingh554@users.noreply.github.com.>
2024-06-17 05:30:00 +0530

    created controllers for frontend auth verification

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 import express from 'express';
-
+import cors from 'cors';
 import dotenv from 'dotenv';
 import { ConnDB } from './config/db.config.js';
 import router from './routes/index.routes.js';
+import cookieParser from 'cookie-parser';
+
+
 
 dotenv.config();
 const app = express();
+app.use(cookieParser());
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    credentials: true 
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- routes/v1/auth.routes.js
+++ routes/v1/auth.routes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { register, login } from '../../controllers/auth.controllers.js'
+import { register, login, authVerify } from '../../controllers/auth.controllers.js'
 
 
 const router = express.Router();
 
+router.route('/verify-token').get(authVerify).post(authVerify);
 router.route('/register').post(register);
 router.route('/login').post(login);
 
