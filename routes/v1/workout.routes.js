c1021d9194be4f2121b2c8702698d33a2a0b664a
harshsingh554 <72743057+harshsingh554@users.noreply.github.com.>
2024-06-25 23:37:10 +0530

    implemented markPreviousWorkoutsAsIncomplete controller

--- routes/v1/workout.routes.js
+++ routes/v1/workout.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {modifyWorkoutStatus,createWorkout, getWorkoutDates, getWorkouts, modifyExerciseStatus, getWorkoutByDate} from "../../controllers/workout.controllers.js";
+import {modifyWorkoutStatus,createWorkout, getWorkoutDates, getWorkouts, modifyExerciseStatus, getWorkoutByDate, markPreviosWorkoutAsIncomplete} from "../../controllers/workout.controllers.js";
 import { authVerify } from "../../middlewares/auth.middleware.js";
 
 const router = express.Router();
@@ -10,4 +10,5 @@ router.route('/modifyWorkoutStatus/:workoutId').patch(modifyWorkoutStatus);
 router.route('/modifyExerciseStatus/:workoutId/:exerciseId').patch(modifyExerciseStatus);
 router.route('/getWorkouts').get(authVerify).get(getWorkouts);
 router.route('/:date').get(authVerify).get(getWorkoutByDate);
+router.route('/markPreviousWorkoutsAsIncomplete').get(authVerify).patch(markPreviosWorkoutAsIncomplete);
 export default router;
