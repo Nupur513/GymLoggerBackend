8d05a101dc04d778c591a4d94687bc155b22b6aa
harshsingh554 <72743057+harshsingh554@users.noreply.github.com.>
2024-06-27 04:03:54 +0530

    intiliased socket functionality

--- routes/v1/workout.routes.js
+++ routes/v1/workout.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {modifyWorkoutStatus,createWorkout, getWorkoutDates, getWorkouts, modifyExerciseStatus, getWorkoutByDate, markPreviosWorkoutAsIncomplete, getWorkoutById} from "../../controllers/workout.controllers.js";
+import {modifyWorkoutStatus,createWorkout, getWorkoutDates, getWorkouts, modifyExerciseStatus, getWorkoutByDate,getExerciseStatus, markPreviosWorkoutAsIncomplete, getWorkoutById} from "../../controllers/workout.controllers.js";
 import { authVerify } from "../../middlewares/auth.middleware.js";
 
 const router = express.Router();
@@ -7,9 +7,10 @@ const router = express.Router();
 router.route('/createWorkout').get(authVerify).post(createWorkout);
 router.route('/getWorkoutDates').get(getWorkoutDates);
 router.route('/modifyWorkoutStatus/:workoutId').patch(modifyWorkoutStatus);
-router.route('/modifyExerciseStatus/:workoutId/:exerciseId').patch(modifyExerciseStatus);
+router.route('/modifyExerciseStatus/:workoutId/:exerciseId').get(authVerify).patch(modifyExerciseStatus);
 router.route('/getWorkouts').get(authVerify).get(getWorkouts);
 router.route('/:date').get(authVerify).get(getWorkoutByDate);
 router.route('/markPreviousWorkoutsAsIncomplete').get(authVerify).patch(markPreviosWorkoutAsIncomplete);
 router.route('/getWorkoutById/:workoutId').get(authVerify).get(getWorkoutById);
+router.route('/getExerciseStatus/:workoutId/:exerciseId').get(authVerify).get(getExerciseStatus);
 export default router;
